d6a7320bb7a940c875d26f906b2a9a0b8abac39d
Zura014 <zuragagnidze50@gmail.com>
2023-10-31 22:41:56 +0400

    headerAlmostDone

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    extend: {
+      fontFamily: {
+        roboto: ["Noto Sans Georgian", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
