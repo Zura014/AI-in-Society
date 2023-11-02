c3515b64685282b99a895cb2f79edfe8259ec28b
Zura014 <zuragagnidze50@gmail.com>
2023-11-02 22:10:55 +0400

    223

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  newClass: boolean = false;
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    
+  } 
+  classChange(newClass: boolean): void{
+    if(newClass == false){
+      newClass=true;
+    }else{
+      newClass=false;
+    }
+    
+  }
 
 }
