1f16d469747d504f4c5bd72a56dc206a9196edad
Zura014 <zuragagnidze50@gmail.com>
2023-11-24 00:55:17 +0400

    addedModules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,6 +23,8 @@ import { ModalTemplateComponent } from './components/components/modal-template/m
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    FontAwesomeModule,
     AppRoutingModule,
     FontAwesomeModule
   ],
